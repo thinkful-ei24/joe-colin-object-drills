7d67f95b34b99fe19db347a23d3497c2572cda5c
Joe-Pena <joepena@protonmail.com>
2018-08-22 10:10:44 -0600

    Object Updater

--- createObjectDrill.js
+++ createObjectDrill.js
@@ -1,3 +1,5 @@
+// Object Creator
+
 function createMyObject() {
   const myObj = {
     foo: 'bar',
@@ -7,7 +9,17 @@ function createMyObject() {
     }
   return myObj;
 }
+//---------------------------------------------------------------------------------------------------
+//Object updater
 
+function updateObject(obj) {
+  obj['foo'] = 'foo';
+  obj['bar'] = 'bar';
+  obj['bizz'] = 'bizz';
+  obj['bang'] = 'bang';
+  return obj;
+}
+//----------------------------------------------------------------------------------------------------
 /* From here down, you are not expected to 
    understand.... for now :)  
 
@@ -55,4 +67,33 @@ function createMyObject() {
  }
  console.log('SUCCESS: Your function works!');
 })();
-
+//--------
+(function testUpdateObject() {
+  var oldObj = {
+    cats: 'cats',
+    dogs: 'dogs',
+  };
+  var newObj = updateObject(oldObj);
+  if (typeof newObj !== 'object') { 
+    console.error('ERROR: `createMyObject` must return an object');
+    return false
+  }
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (!(key in newObj)) {
+      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
+      return false;
+    }
+  });
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (newObj[key] !== key) {
+      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
+      return false;
+    }
+  });
+  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
+    return false;
+  }
+  console.log('SUCCESS: `updateObject` works correctly!');
+  
+})();
