2dce254762676d579955e92a297936e1825e2431
Colin Rupp <colin@thinkful.com>
2018-08-22 11:06:35 -0700

    updated objectDrill2

--- objectDrills2.js
+++ objectDrills2.js
@@ -30,7 +30,31 @@ function enrollInSummerSchool(students) {
   return students;
 }
 // ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
 //
+// findById
+
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
+
+//function findById(items, idNum) {
+//  items.forEach(arrayProperty => {
+//    for (id in arrayProperty) {
+//      if (arrayProperty.id === idNum) {
+//        return arrayProperty
+//      }
+//    }
+//  })
+//}
+
+function findById(items, idNum) {
+  return items.find(x => x.id === idNum)
+}
+
+
 
 // tests
 
@@ -124,3 +148,34 @@ function testIt() {
 }
 
 testIt();
+
+//
+
+function testIt() {
+  const testData = [
+  { id: 1, foo: 'bar' },
+  { id: 2, foo: 'bizz' },
+  { id: 3, bang: 'boo' },
+  ];
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return;
+  }
+  if (result.id !== 3) {
+    console.error(
+        'Asked for item with id of `3` but got back one with id of ' + result.id
+        );
+    return;
+  }
+  if (result.bang !== 'boo') {
+    console.error(
+        'Expected all key/value pairs from target object to be returned'
+        );
+    return;
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
+
